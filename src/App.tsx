0e1baba70a502c11706477c12d92eb6eb672d439
Jota Puerta <jrtatto@gmail.com>
2023-09-19 08:49:05 -0500

    adding google cloud

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ import { Header } from './presentation/components/header/header';
 import { header} from './assets/text/data.json'
 import { CharacterPage } from './presentation/pages/character/character';
 import { footer } from './assets/text/data.json'
+import TagManager from 'react-gtm-module'
+
+console.log(process.env.REACT_APP_GTM_ID)
+
+TagManager.initialize({
+  gtmId: process.env.REACT_APP_GTM_ID as string,
+  auth: process.env.REACT_APP_GTM_AUTH as string,
+  preview: process.env.REACT_APP_GTM_PREVIEW as string,
+})
+
 
 function App() {
   return (
